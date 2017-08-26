4e9e3e0a44964b9d63f6cc4327c609007f31d1d9
Jubair Saidi <jubair.saidi@p3in.com>
2017-08-26 08:02:19 -0400

    Progress on install command. Initial build of an .env file management I call CRUD .env, will prob make it it's own package, it's kinda cool.

--- src/Commands/Install.php
+++ src/Commands/Install.php
@@ -3,6 +3,7 @@
 namespace P3in\Commands;
 
 use Illuminate\Console\Command;
+use P3in\Builders\EnvBuilder;
 
 class Install extends Command
 {
@@ -39,9 +40,50 @@ class Install extends Command
     public function handle()
     {
         $this->info('Lets get started!');
+        $bar = $this->output->createProgressBar(7);
 
+        $this->call('vendor:publish', [
+            '--all' => true,
+        ]);
+        $bar->advance();
+
+        $app_url = $this->ask('What is your app URL?');
+        $bar->advance();
+        $cp_site_name = $this->ask('What is your Control Panel Name?');
+        $bar->advance();
+        $cp_url = $this->getCpUrl();
+        $bar->advance();
+
+        list($cp_site_scheme, $cp_site_host) = explode('://', $cp_url);
+
+        $envBuilder = new EnvBuilder();
+
+        $envBuilder->updateOrCreate([
+            'APP_URL' => $app_url,
+            'ADMIN_WEBSITE_NAME' => $cp_site_name,
+            'ADMIN_WEBSITE_SCHEME' => $cp_site_scheme,
+            'ADMIN_WEBSITE_HOST' => $cp_site_host,
+        ]);
+
+        $bar->advance();
+        $this->info('Starting Migrations (this may take a moment).');
         $this->call('migrate');
+        $bar->advance();
+        $this->info('Running post migration configs.');
         $this->call('passport:install');
-
+        $bar->finish();
     }
+
+    private function getCpUrl()
+    {
+        $cp_url = $this->ask('What is your Control Panel URL?');
+
+        if (filter_var($cp_url, FILTER_VALIDATE_URL) === false) {
+            $this->error('Invalid url.  Make sure you include http://, or https://.');
+            $cp_url = $this->getCpUrl();
+        }
+
+        return $cp_url;
+    }
+
 }
