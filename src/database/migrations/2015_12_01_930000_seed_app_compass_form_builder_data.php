80b6aaa7c30b7ad4af186eeb8166a92b33c32d5b
Jubair Saidi <jubair.saidi@p3in.com>
2017-09-12 12:51:46 -0400

    updated users form to match user validation rules.

--- src/database/migrations/2015_12_01_930000_seed_app_compass_form_builder_data.php
+++ src/database/migrations/2015_12_01_930000_seed_app_compass_form_builder_data.php
@@ -24,15 +24,15 @@ class SeedAppCompassFormBuilderData extends Migration
         ])->firstOrFail();
 
         $users = FormBuilder::new('users', function (FormBuilder $builder) {
-            $builder->string('First Name', 'first_name')->list()->required()->sortable()->searchable();
-            $builder->string('Last Name', 'last_name')->list()->required()->sortable()->searchable();
-            $builder->string('Email', 'email')->list()->validation(['required', 'email', 'unique:users'])->sortable()->searchable();
-            $builder->string('Phone Number', 'phone')->list()->sortable()->searchable();
+            $builder->string('First Name', 'first_name')->list()->validation(['required', 'max:255'])->sortable()->searchable();
+            $builder->string('Last Name', 'last_name')->list()->validation(['required', 'max:255'])->sortable()->searchable();
+            $builder->string('Email', 'email')->list()->validation(['required', 'email', 'unique:users', 'max:255'])->sortable()->searchable();
+            $builder->string('Phone Number', 'phone')->list()->validation(['phone:AUTO,US'])->sortable()->searchable();
             $builder->boolean('Active', 'active')->list()->sortable();
             $builder->string('Created', 'created_at')->list()->edit(false)->sortable()->searchable();
             $builder->string('Updated', 'updated_at')->list()->edit(false)->sortable()->searchable();
             $builder->string('Last Login', 'last_login')->list()->edit(false)->sortable()->searchable();
-            $builder->secret('Password', 'password'); // ->required()
+            $builder->secret('Password', 'password')->validation(['min:6', 'confirmed']); // ->required()
         })->getForm();
 
         Resource::buildAll([
