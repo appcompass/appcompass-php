927d4a924c406db2d4409e8e03d9a813b988f342
Jubair Saidi <jubair.saidi@p3in.com>
2017-09-22 19:23:46 -0400

    added fideloper/proxy to be used since e reverse proxy a lot. fixed noob typo in config file.

--- src/config/app-compass.php
+++ src/config/app-compass.php
@@ -4,4 +4,4 @@ return [
     'admin_site_name'   => env('ADMIN_WEBSITE_NAME', 'App Compass Control Panel'),
     'admin_site_scheme' => env('ADMIN_WEBSITE_SCHEME', 'https'),
     'admin_site_host'   => env('ADMIN_WEBSITE_HOST', 'cp.app-compass.com'),
-]
+];
